42838a4384c92ccf67dc4ea5fa31f56da80eac95
PleyDjessy <pleydjessy.09@gmail.com>
2024-10-19 16:45:15 +0200

    Завершен класс JSONWorker

--- JSONWorker.py
+++ JSONWorker.py
@@ -1,3 +1,6 @@
+import json
+
+
 class JSONWorker:
     def __init__(self, json_data):
         if json_data is list and len(json_data) == 3:
@@ -10,3 +13,15 @@ class JSONWorker:
             print("Некорректный тип данных!")
             raise ValueError
 
+    def create_json_file(self, filename):
+        json_file = f"{filename}.json"
+        with open(json_file, "w", encoding="utf-8") as file:
+            data = json.dumps(self.__json_data, ensure_ascii=False, indent=2)
+            file.write(data)
+        return "Запись успешна"
+
+    @staticmethod
+    def json_reader(filename):
+        with open(filename, "r", encoding="utf-8") as fp:
+            data = json.load(fp)
+        return data
